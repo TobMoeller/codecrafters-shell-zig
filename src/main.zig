9b26dbb150e5e272ed0b17bcc44ca574eb748d18
TobMoeller <tobiasmoellerw@t-online.de>
2025-11-16 00:40:01 +0100

    invalid commands

--- src/main.zig
+++ src/main.zig
@@ -3,7 +3,16 @@ const std = @import("std");
 var stdout_writer = std.fs.File.stdout().writerStreaming(&.{});
 const stdout = &stdout_writer.interface;
 
+var stderr_writer = std.fs.File.stderr().writerStreaming(&.{});
+const stderr = &stderr_writer.interface;
+
+var stdin_buffer: [10*1024]u8 = undefined;
+var stdin_reader = std.fs.File.stdin().readerStreaming(&stdin_buffer);
+const stdin = &stdin_reader.interface;
+
 pub fn main() !void {
     // TODO: Uncomment the code below to pass the first stage
     try stdout.print("$ ", .{});
+    const command = try stdin.takeDelimiter('\n');
+    try stderr.print("{s}: command not found", .{command orelse ""});
 }
