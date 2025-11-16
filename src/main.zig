201c35c4953265637e32097ba8d4971c5995f622
TobMoeller <tobiasmoellerw@t-online.de>
2025-11-17 00:42:43 +0100

    locate executable files

--- src/main.zig
+++ src/main.zig
@@ -47,26 +47,28 @@ const Command = struct {
 };
 
 pub fn main() !void {
-    // TODO: Uncomment the code below to pass the first stage
+    var gpa: std.heap.DebugAllocator(.{}) = .init;
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
     while (true) {
         try stdout.print("$ ", .{});
         const input = try stdin.takeDelimiter('\n') orelse "";
 
         var command: Command = try Command.createFromInput(input);
-        try handleCommand(&command);
+        try handleCommand(allocator, &command);
     }
 }
 
-pub fn handleCommand(command: *Command) !void {
+pub fn handleCommand(allocator: std.mem.Allocator, command: *Command) !void {
     command.builtin = std.meta.stringToEnum(Builtin, command.exec);
     if (command.builtin != null) {
-        return try runBuiltinCommand(command);
+        return try runBuiltinCommand(allocator, command);
     }
 
     try stderr.print("{s}: command not found\n", .{command.exec});
 }
 
-pub fn runBuiltinCommand(command: *Command) !void {
+pub fn runBuiltinCommand(allocator: std.mem.Allocator, command: *Command) !void {
     switch (command.builtin.?) {
         .exit => {
             var exitValue: u8 = 0;
@@ -98,9 +100,61 @@ pub fn runBuiltinCommand(command: *Command) !void {
                 if (std.meta.stringToEnum(Builtin, command.args[0]) != null) {
                     try stdout.print("{s} is a shell builtin\n", .{command.args[0]});
                 } else {
-                    try stdout.print("{s}: not found\n", .{command.args[0]});
+                    try typeFindCommandInPath(allocator, command);
                 }
             }
         }
     }
+}
+
+// TODO refactor naming and structure (add command path to command struct, create find command method)
+pub fn typeFindCommandInPath(allocator: std.mem.Allocator, command: *Command) !void {
+    var env = try std.process.getEnvMap(allocator);
+    defer env.deinit();
+
+    const pathVariable = env.get("PATH") orelse return try typeNotFound(command);
+    var pathIterator = std.mem.splitScalar(u8, pathVariable, ':');
+
+    while (pathIterator.next()) |path| {
+        if (try typeFindCommandInDir(allocator, command, path)) return;
+    } 
+
+    try typeNotFound(command);
+}
+
+pub fn typeFindCommandInDir(allocator: std.mem.Allocator, command: *Command, path: []const u8) !bool {
+    var dir = std.fs.openDirAbsolute(path, .{.access_sub_paths = false, .iterate = true}) catch return false;
+    defer dir.close();
+
+    var dirWalker = try dir.walk(allocator);
+
+    while (true) {
+        const maybe_entry = dirWalker.next() catch continue;
+        const entry = maybe_entry orelse break;
+
+        if (entry.kind == .file and std.mem.eql(u8, command.args[0], entry.basename)) { // TODO handle symlinks
+
+            var file = dir.openFile(entry.basename, .{.mode = .read_only}) catch continue;
+            const fileStat = file.stat() catch continue;
+
+            if (fileStat.mode & std.posix.S.IXUSR != 0) { // TODO handle IXGRP / IXOTH and windows?
+                const absolutePath = try std.fs.path.join(allocator, &.{path, command.args[0]});
+                try stderr.print("{s} is {s}\n", .{command.args[0], absolutePath});
+
+                return true;
+            }
+
+            // try stdout.print("{s} - {b} & {b} = {b}\n", .{entry.basename, fileStat.mode, std.posix.S.IXUSR, fileStat.mode & std.posix.S.IXUSR});
+        }
+    }
+
+    return false;
+}
+
+pub fn typeNotFound(command: *Command) !void {
+    try stderr.print("{s}: not found\n", .{command.args[0]});
+}
+
+pub fn exit(exitValue: u8) void {
+    std.process.exit(exitValue);
 }
